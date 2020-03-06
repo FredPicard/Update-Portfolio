34b8d58fe3d174397bcf6ad5942e1fd07ba7e8b4
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2020-03-06 11:01:39 -0800

    added PHP event handler
    added contact email pop up form

--- Fred_Portfolio/JS/Portfolio.js
+++ Fred_Portfolio/JS/Portfolio.js
@@ -1,22 +1,12 @@
-jQuery(document).ready(function(){   
-    jQuery("a").on('click', function(event) {  
-      if (this.Link !== "") {
-        event.preventDefault();
-        var Link = this.Link
-          jQuery('html, body').animate({
-           scrollTop: jQuery(Link).offset().top
-         }, 2000, function(){
-           window.location.Link = Link;
-        });
-      } 
-    });
- });
 
 
+
+  
 
    function openForm() {
-     document.getElementById("myForm").style.display = "block";
-   }
+    document.getElementById("myForm").style.display = "block";
+  }
+
   
    function closeForm() {
      document.getElementById("myForm").style.display = "none";
